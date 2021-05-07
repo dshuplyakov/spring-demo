a6b606d696ab6b9ba2675bf5895388fe0daf7d41
Artur Gafarov <a.gafarov@galaksion.com>
2021-05-07 15:22:25 +0300

    GET /task/check?id={id}

--- src/main/java/com/example/demo/controller/TaskController.java
+++ src/main/java/com/example/demo/controller/TaskController.java
@@ -2,12 +2,15 @@ package com.example.demo.controller;
 
 import com.example.demo.dto.request.ProcessingRequest;
 import com.example.demo.dto.response.CreatingResult;
+import com.example.demo.dto.response.ProcessingResult;
 import com.example.demo.entity.Task;
 import com.example.demo.repository.TaskRepository;
 import com.example.demo.service.TaskService;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.server.ResponseStatusException;
 import java.util.Optional;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 
 @RestController
@@ -33,7 +36,16 @@ public class TaskController {
     }
 
     @GetMapping("/check")
-    public Optional<Task> checkTask(@RequestParam Long id) {
-        return taskRepository.findById(id);
+    public ProcessingResult checkTask(@RequestParam Long id) {
+        Optional<Task> optionalTask = taskRepository.findById(id);
+
+        if (optionalTask.isEmpty()) {
+            throw new ResponseStatusException(NOT_FOUND, "Task not found");
+        }
+
+        ProcessingResult result = new ProcessingResult();
+        result.setProcessed(optionalTask.get().getProcessed());
+
+        return result;
     }
 }
--- src/main/java/com/example/demo/dto/response/ProcessingResult.java
+++ src/main/java/com/example/demo/dto/response/ProcessingResult.java
@@ -1,4 +1,27 @@
 package com.example.demo.dto.response;
 
+import com.example.demo.entity.Person;
+
 public class ProcessingResult {
+    private Boolean processed;
+
+    private Iterable<Person> persons;
+
+    public Boolean getProcessed() {
+        return processed;
+    }
+
+    public ProcessingResult setProcessed(Boolean processed) {
+        this.processed = processed;
+        return this;
+    }
+
+    public Iterable<Person> getPersons() {
+        return persons;
+    }
+
+    public ProcessingResult setPersons(Iterable<Person> persons) {
+        this.persons = persons;
+        return this;
+    }
 }
